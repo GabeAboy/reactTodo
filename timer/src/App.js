cbb2db288ca9c43eaebbb26e2d356f9deeb03b37
Merksue <gabeaboy@gmail.com>
2017-01-15 09:45:33 -0500

    pomodoro research,and timer lifecycle research

---
+++ timer/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import Clock from './Clock.js'
+
+class App extends Component {
+  render() {
+    return (
+
+      <Clock/>
+
+
+    );
+  }
+}
+
+export default App;
